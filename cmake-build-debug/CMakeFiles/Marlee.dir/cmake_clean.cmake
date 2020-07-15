8e7d9d5aedbd7d2d121308ba7c02e8fdb2e2584c
KreuzBe <kreuzbe@web.de>
2020-07-15 22:03:27 +0200

    .idea removed

---
+++ cmake-build-debug/CMakeFiles/Marlee.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Marlee.dir/main.cpp.obj"
+  "Marlee.exe"
+  "Marlee.exe.manifest"
+  "Marlee.lib"
+  "Marlee.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Marlee.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
