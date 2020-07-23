b46442993c3733d7e6f4699ce6d0a1271827d430
KreuzBe <kreuzbe@web.de>
2020-07-23 10:39:14 +0200

    draw triangle

--- main.cpp
+++ main.cpp
@@ -6,21 +6,24 @@
 int main() {
 
   const char *vertexShaderSource =
-      "#version 330 core\n"
-      "layout (location = 0) in vec3 aPos;\n"
-      "void main()\n"
-      "{\n"
-      "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-      "}\0";
+      R"(
+#version 330 core
+layout (location = 0) in vec3 aPos;
+void main() {
+   gl_Position = vec4(aPos, 1.0);
+}
+)";
 
   const char *fragmentShaderSource =
-      "#version 330 core\n"
-      "out vec4 FragColor;\n"
-      "\n"
-      "void main()\n"
-      "{\n"
-      "    FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
-      "} ";
+      R"(
+#version 330 core
+out vec4 FragColor;
+
+void main()
+{
+  FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);
+}
+)";
 
   GLFWwindow *window = nullptr;
 
@@ -37,7 +40,16 @@ int main() {
   if (glewInit() != GLEW_OK)
     return -1;
 
-  float vertices[] = {-0.5f, -0.5f, 0.0f, 0.5f, -0.5f, 0.0f, 0.0f, 0.5f, 0.0f};
+  float vertices[] = {
+      -0.5f, -0.5f,
+      0.0f, //
+      0.5f,  -0.5f,
+      0.0f, //
+      0.0f,  0.5f,
+      0.0f //
+           //
+
+  };
 
   unsigned int VBO;
 
@@ -96,9 +108,23 @@ int main() {
   glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
   glEnableVertexAttribArray(0);
 
+  unsigned int VAO;
+  glGenVertexArrays(1, &VAO);
+
+  glBindVertexArray(VAO);
+  glBindBuffer(GL_ARRAY_BUFFER, VBO);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
+  glEnableVertexAttribArray(0);
+
   while (!glfwWindowShouldClose(window)) {
     glClearColor(.25, 1, 0.5, .5);
     glClear(GL_COLOR_BUFFER_BIT);
+
+    glUseProgram(shaderProgram);
+    glBindVertexArray(VAO);
+    glDrawArrays(GL_TRIANGLES, 0, 3);
 
     glfwSwapBuffers(window);
 
